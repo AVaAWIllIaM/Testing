b4f65e9b464b9ddd603ec0b6142550de62588f9b
AVaAWIllIaM <Benjamin.lipiridi@gmail.com>
2025-04-07 16:07:32 +0300

    Final 0.3

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import fuzion as fu
-
-text = input("Ведите текст\nВвод\n")
-
-qwest = input("Поиск по букве(a) или словарь(b)?\nВвод\n")
-if qwest == "a":
-    fu.tor(text)
-elif qwest == 'b':
-    print(fu.analize_text(text))
-else:
+import sys
+try:
+    if len(sys.argv) > 1:
+        with open(sys.argv[1], 'r') as file:
+            text = file.read()
+    else:
+        text = input("Ведите текст\nВвод\n")
+    qwest = input("Поиск по букве(a) или словарь(b)?\nВвод\n")
+    if qwest == "a":
+        fu.tor(text)
+    elif qwest == 'b':
+        print(fu.analize_text(text))
+    else:
+        print("Ошибка повтори")
+except:
     print("Ошибка повтори")
